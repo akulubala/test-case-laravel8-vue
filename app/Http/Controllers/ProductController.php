0347a795ca3c0da4f85fb0d1fda73fa743cb9034
Raymond Cheng <akulubala@gmail.com>
2021-11-07 20:58:22 +0800

    init project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(10);
+
+        return Inertia::render('Post/Index', ['products' => $products]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Post/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Product::create(
+            $request->validate([
+                'title' => ['required', 'max:90'],
+                'description' => ['required'],
+            ])
+        );
+
+        return Redirect::route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return Inertia::render('Post/Edit', [
+            'post' => [
+                'id' => $product->id,
+                'title' => $product->title,
+                'description' => $product->description
+            ]
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $data = $request->validate([
+            'title' => ['required', 'max:90'],
+            'description' => ['required'],
+        ]);
+        $product->update($data);
+    
+
+        return Redirect::route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($product)
+    {
+        $product->delete();
+        
+        return Redirect::route('products.index');
+    }
+}
